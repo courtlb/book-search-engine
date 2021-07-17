2a27cf9c84b9d331e5835607d09ada160570b465
Courtney Brown <clbrown0607@gmail.com>
2021-07-17 12:32:55 -0500

    created queries.js

---
+++ client/src/utils/queries.js
@@ -0,0 +1,19 @@
+import { gql } from '@apollo/client';
+
+export const GET_ME = gql`
+    {
+        user {
+            username
+            email
+            bookCount
+            savedBooks {
+                bookId
+                authors
+                description
+                title
+                image
+                link
+            }
+        }
+    }
+`;
